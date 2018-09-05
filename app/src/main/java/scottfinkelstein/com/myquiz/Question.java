f5201f0f702d0a4d5bb0d033eae1c41a4a23b83f
Scott Finkelstein <sbf0202@gmail.com>
2018-09-05 10:51:01 -0400

    Added Question class for Next Week's assignment.

---
+++ app/src/main/java/scottfinkelstein/com/myquiz/Question.java
@@ -0,0 +1,30 @@
+package scottfinkelstein.com.myquiz;
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean getAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
+
+
